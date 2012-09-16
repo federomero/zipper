c21618c7c90bf9f5c0a4ec2a3eeaf84e0defc75e
Federico Romero <hi@federomero.uy>
2012-09-16 13:13:32 -0300

    Small refactor and name changes

--- zipper.go
+++ zipper.go
@@ -8,37 +8,35 @@ import (
   "strings"
 )
 
-type download struct {
+type URLDownload struct {
   url  string
   body io.ReadCloser
   err  error
 }
 
-func CreateZip(urls []string, fo io.Writer) error{
+// Download all files from the given urls
+// and write them on the output
+func CreateZip(urls []string, output io.Writer) error{
 
   buf := new(bytes.Buffer)
 
   archive := zip.NewWriter(buf)
 
-  c := make(chan download, len(urls))
+  c := make(chan URLDownload, len(urls))
 
   for _, url := range urls{
     go downloadURL(url, c)
   }
 
   for i := 0; i < len(urls); i++ {
-    d := <-c
-    if d.err != nil { return d.err }
-    fileName := fileNameFromURL(d.url)
-    w, err := archive.Create(fileName)
+    err := appendToArchive(archive, <-c)
     if err != nil { return err }
-    _, err = io.Copy(w, d.body)
   }
 
   err := archive.Close()
   if err != nil { return err }
 
-  _, err = buf.WriteTo(fo)
+  _, err = buf.WriteTo(output)
   return err
 }
 
@@ -47,12 +45,22 @@ func fileNameFromURL(url string) string{
   return a[len(a)-1]
 }
 
-func downloadURL(url string, c chan download){
+func downloadURL(url string, c chan URLDownload){
   resp, err := http.Get(url)
   if err != nil {
-    c <- download{url, nil, err}
+    c <- URLDownload{url, nil, err}
   }else{
-    c <- download{url, resp.Body, err}
+    c <- URLDownload{url, resp.Body, err}
   }
 
+}
+
+func appendToArchive(archive *zip.Writer, download URLDownload) error{
+  if download.err != nil { return download.err }
+  fileName := fileNameFromURL(download.url)
+  w, err := archive.Create(fileName)
+  if err != nil { return err }
+  _, err = io.Copy(w, download.body)
+  defer download.body.Close()
+  return nil
 }
